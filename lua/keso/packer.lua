103ddae83ed2158f71e2da0d16124e8eb801ef55
Dorde Kesic <dordekesic.keso@gmail.com>
2023-09-13 19:08:20 +0200

    removes unnecessary curly braces

--- lua/keso/packer.lua
+++ lua/keso/packer.lua
@@ -20,5 +20,5 @@ return require('packer').startup(function(use)
 		end
 	})
         
-	use({'nvim-treesitter/nvim-treesitter', {run=':TSUpdate'}})
+	use('nvim-treesitter/nvim-treesitter', {run=':TSUpdate'})
 end)
