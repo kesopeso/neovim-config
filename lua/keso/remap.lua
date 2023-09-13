01f19a48cf42e083e8f72fac66de96d178349edb
Dorde Kesic <dordekesic.keso@gmail.com>
2023-09-13 19:03:00 +0200

    removes unnecessary remaps for copying to/from clipboard, adds treesitter plugin

--- lua/keso/remap.lua
+++ lua/keso/remap.lua
@@ -3,9 +3,9 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 -- dependencies
 -- sudo apt-get intsall xclip
 vim.cmd("set clipboard=unnamedplus")
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>p", '"+p')
-vim.keymap.set("v", "<leader>P", '"+P')
-vim.keymap.set("n", "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>P", '"+P')
+-- vim.keymap.set("v", "<leader>y", '"+y')
+-- vim.keymap.set("v", "<leader>p", '"+p')
+-- vim.keymap.set("v", "<leader>P", '"+P')
+-- vim.keymap.set("n", "<leader>p", '"+p')
+-- vim.keymap.set("n", "<leader>P", '"+P')
 
