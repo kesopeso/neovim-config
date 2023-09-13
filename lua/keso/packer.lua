2c1ca90db87a99180fe9f590a0ad33f02a684426
Dorde Kesic <dordekesic.keso@gmail.com>
2023-09-13 19:18:02 +0200

    adds undotree plugin

--- lua/keso/packer.lua
+++ lua/keso/packer.lua
@@ -22,4 +22,7 @@ return require('packer').startup(function(use)
         
 	use('nvim-treesitter/nvim-treesitter', {run=':TSUpdate'})
 	use('nvim-treesitter/playground')
+
+	use('mbbill/undotree')
 end)
+
