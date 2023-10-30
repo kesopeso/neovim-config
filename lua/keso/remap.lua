daab62a766e0843dd584dfaa034e37da97e81620
Dorde Kesic <dordekesic.keso@gmail.com>
2023-10-30 09:30:41 +0100

    adds window splitting, <leader>s for normal split and <leader>vs for vertical split

--- lua/keso/remap.lua
+++ lua/keso/remap.lua
@@ -46,4 +46,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-k>", "<C->>k")
+
+-- window splitting
+vim.keymap.set("n", "<leader>s", ":split<CR>")
+vim.keymap.set("n", "<leader>vs", ":vsplit<CR>")
