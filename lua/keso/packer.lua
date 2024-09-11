dd9dd84d75466574ea9d4765af8ba67ae27e3f77
Dorde Kesic <dordekesic.keso@gmail.com>
2024-09-11 13:40:58 +0200

    removes tsserver from lsp ensure installed list

--- lua/keso/packer.lua
+++ lua/keso/packer.lua
@@ -1,82 +1,83 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
-    -- fuzzy find files
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-
-    -- color scheme
+	-- fuzzy find files
 	use({
-		'rose-pine/neovim', as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
-    -- code formatter / "color maker"
-	use('nvim-treesitter/nvim-treesitter', {run=':TSUpdate'})
-	use('nvim-treesitter/playground')
+	-- color scheme
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 
-    -- file undo history
-	use('mbbill/undotree')
+	-- code formatter / "color maker"
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
 
-    -- git tracker
-	use('tpope/vim-fugitive')
+	-- file undo history
+	use("mbbill/undotree")
 
-    -- language server protocol (intellisense, autocomplete, etc etc)
-	use {
-		'VonHeikemen/lsp-zero.nvim',
+	-- git tracker
+	use("tpope/vim-fugitive")
+
+	-- language server protocol (intellisense, autocomplete, etc etc)
+	use({
+		"VonHeikemen/lsp-zero.nvim",
 		requires = {
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
 			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-    -- file browser
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = { { 'nvim-tree/nvim-web-devicons' } }
-    }
+	-- file browser
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = { { "nvim-tree/nvim-web-devicons" } },
+	})
 
-    -- info line
-    use 'nvim-lualine/lualine.nvim'
+	-- info line
+	use("nvim-lualine/lualine.nvim")
 
-    -- surround replacement
-    use({
-        'kylechui/nvim-surround', tag = '*',
-        config = function()
-            require('nvim-surround').setup({})
-        end
-    })
+	-- surround replacement
+	use({
+		"kylechui/nvim-surround",
+		tag = "*",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	})
 
-    -- html tags completion
-    use 'mattn/emmet-vim'
+	-- html tags completion
+	use("mattn/emmet-vim")
 
-    -- formatting
-    use 'mhartington/formatter.nvim'
+	-- formatting
+	use("mhartington/formatter.nvim")
 end)
-
