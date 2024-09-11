f8683e08b9acf6a57b80b86be841e3630b9617d1
Dorde Kesic <dordekesic.keso@gmail.com>
2024-09-11 13:48:33 +0200

    formats treesitter.lua file

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,21 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c_sharp", "javascript", "typescript", "json", "yaml", "dockerfile", "html", "css", "scss", "c", "lua", "vim", "vimdoc", "query" },
+	ensure_installed = {
+		"c_sharp",
+		"javascript",
+		"typescript",
+		"json",
+		"yaml",
+		"dockerfile",
+		"html",
+		"css",
+		"scss",
+		"c",
+		"lua",
+		"vim",
+		"vimdoc",
+		"query",
+	},
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -18,4 +33,4 @@ require'nvim-treesitter.configs'.setup {
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
-}
+})
