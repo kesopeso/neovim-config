874c7c8a72d2e69006387c10fabdf5a6078390e1
Dorde Kesic <dordekesic.keso@gmail.com>
2024-09-12 10:12:16 +0200

    removes modifying the color scheme's default background for an active window

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,10 @@
 function UseColorScheme(color)
-	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none", default = true })
+	-- vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = "none", default = true })
 end
 
-UseColorScheme()
+-- available: rose-pine, tokyonight
+UseColorScheme("tokyonight")
